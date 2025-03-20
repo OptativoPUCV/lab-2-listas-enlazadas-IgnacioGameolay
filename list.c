5a1edc4b451052770089a6a146336d6069d3ff56
Your Name <you@example.com>
2025-03-20 18:29:39 -0300

    new try

--- list.c
+++ list.c
@@ -28,45 +28,69 @@ Node * createNode(void * data) {
     return new;
 }
 
+// Crea una lista vacía. Inicializa los punteros head, tail y current a NULL.
 List * createList() {
+    // Reserva memoria para una nueva lista.
     List* newList = (List*) malloc(sizeof(List));
 
+    // Inicializa los punteros de la lista: cabeza (head), cola (tail) y el actual (current) en NULL.
     newList->head = NULL;
     newList->tail = NULL;
     newList->current = NULL;
 
+    // Devuelve la lista vacía creada.
     return newList;
 }
 
+// Coloca el puntero current en el primer nodo de la lista y devuelve su dato.
 void * firstList(List * list) {
+    // Si la lista está vacía, devuelve NULL.
     if (list == NULL || list->head == NULL) {
         return NULL;
     }
+    // Si la lista no está vacía, coloca current en el primer nodo.
     list->current = list->head;
+
+    // Devuelve el dato del primer nodo.
     return list->current->data;
 }
 
+// Mueve el puntero current al siguiente nodo de la lista y devuelve su dato.
 void * nextList(List * list) {
+    // Si la lista o el nodo actual no existen o el siguiente nodo es NULL, devuelve NULL.
     if (list == NULL || list->current == NULL || list->current->next == NULL) {
         return NULL;
     }
+    // Mueve el puntero current al siguiente nodo.
     list->current = list->current->next;
+
+    // Devuelve el dato del nodo siguiente.
     return list->current->data;
 }
 
+// Coloca el puntero current en el último nodo de la lista y devuelve su dato.
 void * lastList(List * list) {
+    // Si la lista está vacía o no tiene un último nodo, devuelve NULL.
     if (list == NULL || list->tail == NULL) {
         return NULL;
     }
+    // Si la lista no está vacía, coloca current en el último nodo.
     list->current = list->tail;
+
+    // Devuelve el dato del último nodo.
     return list->current->data;
 }
 
+// Mueve el puntero current al nodo anterior de la lista y devuelve su dato.
 void * prevList(List * list)  {
+    // Si la lista o el nodo actual no existen o el nodo anterior es NULL, devuelve NULL.
     if (list == NULL || list->current == NULL || list->current->prev == NULL) {
         return NULL;
     }
+    // Mueve el puntero current al nodo anterior.
     list ->current = list->current->prev;
+
+    // Devuelve el dato del nodo anterior.
     return list->current->data;
 }
 
